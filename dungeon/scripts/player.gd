3cc99fa0fd05192064d64c538ae9e9e65b1e1fea
IceVeryChill <108781488+Stanlee1234@users.noreply.github.com>
2026-02-28 13:53:31 -0800

    yay player kinda working

--- dungeon/scripts/player.gd
+++ dungeon/scripts/player.gd
@@ -1,30 +1,37 @@
 extends CharacterBody2D
 
-
 const SPEED = 100.0
 const JUMP_VELOCITY = -200.0
 
 var DEAD = false
 
+@onready var sprite = $AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
 	if not DEAD:
-		# Add the gravity.
 		if not is_on_floor():
 			velocity += get_gravity() * delta
 
-		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		if Input.is_action_just_pressed("ui_up") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
 		var direction := Input.get_axis("ui_left", "ui_right")
 		if direction:
 			velocity.x = direction * SPEED
+			sprite.flip_h = direction < 0
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 
+		_update_animations(direction)
 		move_and_slide()
+
+func _update_animations(direction):
+	if not is_on_floor():
+		sprite.play("jump")
+	elif direction != 0:
+		sprite.play("walk")
+	else:
+		sprite.play("idle")
 		
 func _die():
 	if DEAD: 
@@ -32,7 +39,6 @@ func _die():
 	DEAD = true
 	
 	set_physics_process(false) 
-	get_tree().paused = false
 	call_deferred("_reload_game")
 
 func _reload_game():
