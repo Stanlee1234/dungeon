7ca328db4ff41538d07e7e1a76a81f2e954697ef
IceVeryChill <108781488+Stanlee1234@users.noreply.github.com>
2026-02-28 13:33:11 -0800

    fire

--- dungeon/scripts/player.gd
+++ dungeon/scripts/player.gd
@@ -28,5 +28,5 @@ func _physics_process(delta: float) -> void:
 		
 func _die():
 	DEAD = true
-	await get_tree().create_timer(0).timeout
+	await get_tree().create_timer(0.5).timeout
 	get_tree().reload_current_scene()
